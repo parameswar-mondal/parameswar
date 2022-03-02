cf9158cb8d7181d5aa18633a62514529c0e1f5d4
Parameswar Mondal <param23.03@gmail.com>
2022-03-02 15:16:29 +0530

    first changes

--- index.js
+++ index.js
@@ -1,8 +1,8 @@
 function aboutLibrary() {
-    const aboutMe = `Hi, I am Parameswar Mondal. This is my first npm package. I will try for different but now it is for testing..`;
+    const aboutMe = `Hi, I am Parameswar Mondal. This is my first npm package. As of now it is for testing..`;
     return aboutMe;
 }
 
 module.exports = {
-    aboutLibrary: aboutLibrary
+    aboutLibrary
 }
